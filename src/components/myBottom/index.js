dd1bfef50429b4c01a2854dc3cd222c23265990e
mengleng <18337952261@163.com>
2021-06-04 19:02:37 +0800

    细节更新

--- src/components/myBottom/index.js
+++ src/components/myBottom/index.js
@@ -17,8 +17,8 @@ function myBottom (props) {
       </div>
       <div className="bottom-right">
         <div className="term-privacy">
-          <a href="https://daostarter.oss-cn-hangzhou.aliyuncs.com/DAOSTARTER%20TERMS%20AND%20CONDITIONS.pdf" >Terms</a>
-          <a href="https://daostarter.oss-cn-hangzhou.aliyuncs.com/PRIVACY%20POLICY.pdf" >Privacy</a>
+          <a href="https://daostarter.pro/DAOSTARTER%20TERMS%20AND%20CONDITIONS.pdf" >Terms</a>
+          <a href="https://daostarter.pro/PRIVACY%20POLICY.pdf" >Privacy</a>
         </div>
         <div className="icon">
           {
